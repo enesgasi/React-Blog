1fbb7416c251c9b7a23524399fb5f5301ea22c6e
enesgasi <enesgasi@hotmail.com>
2022-11-02 11:09:23 +0300

    ufak şeyler

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -8,8 +8,8 @@ const BlogList = (props) => {
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
                     <p> { blog.body }</p>
+                    <p>Written by { blog.author }</p>
                 </div>
             ))}
         </div>
